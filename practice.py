cd05800a2da4682f9f97afbb77ee6e4d13bb7417
ashok-9 <ashokbommena9@gmail.com>
2023-03-06 02:30:59 +0530

    lc

--- practice.py
+++ practice.py
@@ -1,9 +1,1 @@
-class Solution:
-    def transpose(self, matrix):
-        m=[[0 for i in range(len(matrix))] for i in range(len(matrix[0]))]
-        
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                m[j][i]=matrix[i][j]
-        return m
-s=Solution()
+print("hello world")
